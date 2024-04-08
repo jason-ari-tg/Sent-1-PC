a79d35aef23559e319a4eb04010e8da966cfe846
jason-ari-tg <159946855+jason-ari-tg@users.noreply.github.com>
2024-04-08 12:32:52 -0400

    Update s1.ps1

--- s1.ps1
+++ s1.ps1
@@ -5,3 +5,13 @@ Invoke-WebRequest -Uri $url -OutFile $filePath
 
 # Install the package silently
 Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$filePath`" /qn /norestart SITE_TOKEN=eyJ1cmwiOiAiaHR0cHM6Ly91c2VhMS0wMTcuc2VudGluZWxvbmUubmV0IiwgInNpdGVfa2V5IjogImdfMWI3ODYzMDdkYzExNmYzYiJ9" -Wait
+
+# Check if the application is running
+$appName = "Sentinel"
+$process = Get-Process | Where-Object { $_.Name -eq $appName }
+
+if ($process) {
+    Write-Host "$appName is installed and running."
+} else {
+    Write-Host "$appName is installed but not running."
+}
